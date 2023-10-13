243b67da9b37d1e38f41bd885ffde33a1e761e38
Tyuiu.SavitskyDA@outlook.com <Tyuiu.SavitskyDA@outlook.com>
2023-10-13 21:05:13 +0500

    Task1

---
+++ Tyuiu.SavitskyDA.Sprint2.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.SavitskyDA.Sprint2.Task1.V17.Lib
+{
+    public class DataService : ISprint2Task1V17
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            //==, !=, <, >, <=, >=, последовательность можно чередовать, но использовать один раз в выражении
+            //|, &, ||, &&, !, ^, последовательность операций не должна нарушаться
+            //a = 135, b = 123, c = 455, d = 321 
+            // 0True, 1False, 2True, 3True, 4True, 5False
+            bool[] res = new bool[6];
+            res[0] = (a > b) | (c < d);
+            res[1] = (a < b) & (c > d);
+            res[2] = (a > b) || (c < d);
+            res[3] = (a > b) && (c > d);
+            res[4] = !(!res[0]);
+            res[5] = (a > b) ^ (c > d);
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.SavitskyDA.Sprint2.Task1.V17.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SavitskyDA.Sprint2.Task1.V17.Lib;
+
+namespace Tyuiu.SavitskyDA.Sprint2.Task1.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void GetLogicOperations()
+        {
+            //Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность можно чередовать,
+            //но использовать один раз в выражении) и логических операций (|, &, ||, &&, !, ^,
+            //последовательность операций не должна нарушаться), а также арифметических выражений,
+            //которая вернет логическую последовательность(массив): (True, False, True, True, True, False),
+            //при a = 135, b = 123, c = 455, d = 321
+            DataService ds = new DataService();
+            int a = 135;
+            int b = 123;
+            int c = 455;
+            int d = 321;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = new bool[6] { true, false, true, true, true, false };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
